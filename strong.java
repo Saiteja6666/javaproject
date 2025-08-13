792d5b1abb2dac75d245ff66cbfe11a0ddd43aa8
Saiteja6666 <teja26770@gmail.com>
2025-08-13 10:32:39 +0530

    add this file

--- strong.java
+++ strong.java
@@ -1,31 +1,52 @@
-class Strong_number
+class strong
 {
-	public static void main(String[] args)
+public static void main(String[] args)
 	{
-		for (int i = 1; i <= 5000; i++)
-		{
-			if (isItStrong(i))
-			{			
-				System.out.println(i);
-			}
-		}
-	}
-	static boolean isItStrong(int num) {
-		int no = num;
-		int sum = 0;
-		while (no > 0) {
-			int digit = no % 10;
-			sum += fact(digit);
- 
-			no = no / 10;
-		}
-		return sum == num;
-	}
-	static int fact(int digit) {
-		int f = 1;
-		for (int j = digit; j > 1; j--) {
-			f *= j;
-		}
-		return f;
-	}
+System.out.println("Strong numbers from 1 to 50000 are :");
+for (int i = 1; i <= 50000; i++)
+{
+if (isItStrong(i))
+{			
+System.out.println(i);
 }
+}
+}
+static boolean isItStrong(int num)
+{
+int no = num;
+int sum = 0;
+while (no > 0)
+{
+int digit = no % 10;
+sum += fact(digit);
+ no = no / 10;
+}
+return sum == num;
+}
+static int fact(int digit)
+{
+int f = 1;
+for (int j = digit; j > 1; j--)
+{
+f *= j;
+}
+return f;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+	
